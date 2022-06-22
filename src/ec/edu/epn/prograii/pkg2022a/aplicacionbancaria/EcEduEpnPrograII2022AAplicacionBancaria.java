79bf31481629672bb090fe8287bd6374d81b8bcd
DilanAndrade007 <dilan.andrade@epn.edu.ec>
2022-06-22 16:52:27 -0500

    commit de prueba

--- src/ec/edu/epn/prograii/pkg2022a/aplicacionbancaria/EcEduEpnPrograII2022AAplicacionBancaria.java
+++ src/ec/edu/epn/prograii/pkg2022a/aplicacionbancaria/EcEduEpnPrograII2022AAplicacionBancaria.java
@@ -15,6 +15,10 @@ public class EcEduEpnPrograII2022AAplicacionBancaria {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        if (true) {
+            System.out.println("verdadero"+"prueba");
+                    
+        }
     }
     
 }
