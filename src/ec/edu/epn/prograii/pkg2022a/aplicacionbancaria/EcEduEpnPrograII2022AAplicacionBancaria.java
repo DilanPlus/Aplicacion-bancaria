5a9b89eedfda2894927f4bc3a2e8769a0799f509
DilanAndrade007 <dilan.andrade@epn.edu.ec>
2022-06-22 16:31:16 -0500

    cambios

---
+++ src/ec/edu/epn/prograii/pkg2022a/aplicacionbancaria/EcEduEpnPrograII2022AAplicacionBancaria.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ec.edu.epn.prograii.pkg2022a.aplicacionbancaria;
+
+/**
+ *
+ * @author DilanPlus
+ */
+public class EcEduEpnPrograII2022AAplicacionBancaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
