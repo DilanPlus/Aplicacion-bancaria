7994d917ae5ca064b9c8b107e18947918415d088
DilanAndrade007 <dilan.andrade@epn.edu.ec>
2022-06-23 18:03:43 -0500

    main vacio

--- src/ec/edu/epn/prograii/pkg2022a/aplicacionbancaria/EcEduEpnPrograII2022AAplicacionBancaria.java
+++ src/ec/edu/epn/prograii/pkg2022a/aplicacionbancaria/EcEduEpnPrograII2022AAplicacionBancaria.java
@@ -15,9 +15,6 @@ public class EcEduEpnPrograII2022AAplicacionBancaria {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        if (true) {
-            System.out.println("verdadero");       
-        }
+        
     }
-    //si vale xd
 }
