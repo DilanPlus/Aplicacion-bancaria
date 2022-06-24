8b1137315111015774cab35c2b74bd7f96ad35dc
DilanAndrade007 <dilan.andrade@epn.edu.ec>
2022-06-23 22:15:49 -0500

    ventana de detalle completa y ventana tranferencia en proceso

--- src/ec/edu/epn/prograii/pkg2022a/aplicacionbancaria/VentanaTranferencia.java
+++ src/ec/edu/epn/prograii/pkg2022a/aplicacionbancaria/VentanaTranferencia.java
@@ -9,7 +9,8 @@ package ec.edu.epn.prograii.pkg2022a.aplicacionbancaria;
  * @author DilanPlus
  */
 public class VentanaTranferencia extends javax.swing.JFrame {
-
+    VentanaDetalle hola=new VentanaDetalle();
+    
     /**
      * Creates new form VentanaTranferencia
      */
@@ -26,21 +27,40 @@ public class VentanaTranferencia extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(169, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(156, 156, 156))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(133, 133, 133)
+                .addComponent(jButton1)
+                .addContainerGap(145, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +98,6 @@ public class VentanaTranferencia extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
